a8ef884562c4a0b76650dbb968e4660bd8612764
adarshgupta-2003 <adarshguptaa.dev@gmail.com>
2024-02-09 00:35:49 +0530

    bug-fix: margin

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function showSideBar() {
     const mobileBar = document.querySelector('.mobile-sidebar');
+    // allContent.style.display = 'none';
     mobileBar.style.display="flex"
 }
 
 function closeSideBar() {
     const mobileBar = document.querySelector('.mobile-sidebar');
+    // allContent.style.display = 'block';
     mobileBar.style.display="none"
 }
 
