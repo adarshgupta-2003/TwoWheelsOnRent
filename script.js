12407affd9b640c433c0516ec318d77eb5bf07b7
adarshgupta-2003 <adarshguptaa.dev@gmail.com>
2024-03-15 14:38:30 +0530

    feat: backend

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ function closeSideBar() {
     mobileBar.style.display="none"
 }
 
+
+// redirect to login page if not logged in
+
+const userInfo = sessionStorage.getItem("user-info");
+const userName = localStorage.getItem("username");
+// || !userName
+// if (!userInfo) {
+//     window.location.href = "./login.html";
+// }
+
+function postSignedOut(){
+    var popup1 = document.querySelector('.loggedIn1');
+    var popup2 = document.querySelector('.loggedIn2');
+    const greet = document.getElementById("greet");
+    greet.style.display = 'none';
+    popup1.style.display = 'flex';
+    popup2.style.display = 'flex';
+}
+
+function signOut() {
+    // sessionStorage.removeItem('user-creds');
+    // sessionStorage.removeItem('user-info');
+    sessionStorage.clear();
+    localStorage.clear();
+    // closePopup();
+    postSignedOut();
+    console.log("session removed");
+  }
+
+  function checkIfloggedIn(){
+    window.location.href = "login.html";
+  }
+
+
